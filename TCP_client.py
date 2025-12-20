93eefe4ac0f78ef2ed0d891d58af9d00d676f903
quynhneo <qmn203@nyu.edu>
2025-12-20 16:55:26 -0500

    finished testing tcp proof

--- TCP_client.py
+++ TCP_client.py
@@ -1,12 +1,16 @@
 import socket
 import time
 
+
 CRIO_IP = "192.164.1.2"
-PORT = 5005
+#CRIO_IP = "127.0.0.1"
+PORT = 5010
 
 with socket.create_connection((CRIO_IP, PORT)) as s:
     val = False
     while True:
         val = not val
+        print("sending", 1 if val else 0)
         s.sendall(b"\x01" if val else b"\x00")
-        time.sleep(0.5)
+        time.sleep(1)
+
